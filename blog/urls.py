f427bc7ab8486f273722e62257ddda42bf416786
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-19 21:01:17 +0300

    //

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from .views import index, about, contact
 
 urlpatterns = [
     path('home', index, name='home'),
-    path('about', about, name='about'),
-    path('contact', contact, name='contact'),
+    path('about', about, name='about', kwargs={'name':'Egor', 'age':25}),
+    path('contact/<str:street>/<str:building>/', contact, name='contact'),
+    path('contact/', contact, name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,17 @@ def index(request):
     return HttpResponse('<h2>Главная</h2>')
 
 
-def about(request):
-    return HttpResponse('<h2>О сайте</h2>')
+def about(request, name, age):
+    return HttpResponse(f'''
+    <h2>О пользователе</h2>
+    <p>Имя: {name}</p>
+    <i>Возраст: {age}</i>
+    ''')
 
 
-def contact(request):
-    return HttpResponse('<h2>Контакты</h2>')
+def contact(request, street : str = 'Undefined', building : str = 'Null'):
+    return HttpResponse(f'''
+    <h2>Наши контакты</h2>
+    <p>Мы находимся на улице : {street}</p>
+    <i>Номер строения : {building}</i>
+    ''')
