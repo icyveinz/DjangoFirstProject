637d761c27142a840004ed7e9c17e4d7dde9158c
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-20 20:58:22 +0300

    //

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    header = "Данные пользователя"  # обычная переменная
-    langs = ["Python", "Java", "C#"]  # список
-    user = {"name": "Tom", "age": 23}  # словарь
-    address = ("Абрикосовая", 23, 45)  # кортеж
-
-    data = {"header": header, "langs": langs, "user": user, "address": address}
+    data = {
+        "age" : 66,
+        "languages" : ["python", "java"]
+    }
     return render(request, "blog/index.html", context=data)
