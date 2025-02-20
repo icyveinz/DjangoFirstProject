e5a47aed285249d10b3aa6e09c17e5daff0f9553
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-20 20:13:19 +0300

    // added template

--- blog/views.py
+++ blog/views.py
@@ -1,37 +1,5 @@
-from django.http import HttpResponse
-
-
-# def index(request):
-#     return HttpResponse('<h2>Главная</h2>')
-#
-#
-# def about(request, name, age):
-#     return HttpResponse(f'''
-#     <h2>О пользователе</h2>
-#     <p>Имя: {name}</p>
-#     <i>Возраст: {age}</i>
-#     ''')
-#
-#
-# def contact(request, street : str = 'Undefined', building : str = 'Null'):
-#     return HttpResponse(f'''
-#     <h2>Наши контакты</h2>
-#     <p>Мы находимся на улице : {street}</p>
-#     <i>Номер строения : {building}</i>
-#     ''')
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Главная страница")
-
-
-def products(request):
-    return HttpResponse("Список товаров")
-
-
-def new(request):
-    return HttpResponse("Новые товары")
-
-
-def top(request):
-    return HttpResponse("Наиболее популярные товары")
+    return render(request, 'index.html')
