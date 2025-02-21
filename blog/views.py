923f0c57ae8be8b4226e56238b73e1ed8789f85f
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-22 01:41:00 +0300

    //

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "blog/index.html")
+    return render(request, "blog/index.html", context={"site" : "google.com"})
 
 def about(request):
-    return render(request, "blog/about.html")
+    return render(request, "blog/about.html", context={"site" : "yandex.ru"})
