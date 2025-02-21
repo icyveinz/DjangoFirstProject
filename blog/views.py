886bbe496b5bf17751553206042e4b7c9e424137
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-22 01:15:28 +0300

    //

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    data = {
-        "age" : 66,
-        "languages" : ["python", "java"]
-    }
-    return render(request, "blog/index.html", context=data)
+    return render(request, "blog/index.html")
 
 def about(request):
     return render(request, "blog/about.html")
