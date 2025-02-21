f2921ddc39c7c9d9cbc7f8183c4476de18e50e2f
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-22 00:58:34 +0300

    //

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from blog import views
 
 urlpatterns = [
     path('', views.index),
+    path('template_view/',
+         TemplateView.as_view(template_name='blog/index.html', extra_context={
+             "age" : 50,
+             "languages" : ["ss", "vv"]
+         })),
 ]
